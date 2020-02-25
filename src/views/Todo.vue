4fdb36fae15e4dc8de8ab248ee0b6eac48fc00d4
Angelo <angelorpt@gmail.com>
2020-02-25 12:41:47 -0400

    Adicionado axios e reindentado para 4 espaços

---
+++ src/views/Todo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>To Dos List</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
