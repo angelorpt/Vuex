614c5d241448bb9707a43e8d0d4ef019c8a15124
Angelo <angelorpt@gmail.com>
2020-02-25 13:50:02 -0400

    Adicionado materialize-css e botões de ação

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,22 +1,29 @@
 <template>
-    <div>
-        <h1>To Dos List - {{ total }}</h1>
+    <div class="container">
+        <h3>To Dos List Completa</h3>
+        <legend class="right">Total: {{ total }}</legend>
         <div>
             <table>
                 <thead>
                     <th>ID</th>
                     <th>TÍTULO</th>
                     <th>USUÁRIO</th>
+                    <th>Concluir</th>
                 </thead>
                 <tbody>
-                    <tr v-for="todo in lstTodo">
+                    <tr v-for="todo in lstTodo" :key="todo.id" :class="{'blue lighten-5' : todo.completed}">
                         <td>{{ todo.id }}</td>
                         <td>{{ todo.title }}</td>
                         <td>{{ todo.userId }}</td>
+                        <td>
+                            <button v-if="!todo.completed" class="btn blue darken-1 waves-effect">Concluir</button>
+                            <button v-else class="btn grey darken-1 waves-effect">Desfazer</button>
+                        </td>
                     </tr>
                 </tbody>
             </table>
         </div>
+
     </div>
 </template>
 
